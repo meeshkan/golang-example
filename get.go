d5e4f252ee9b5762ebf4d125950eb6cb7db307f8
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-10-22 12:41:25 +0300

    Test: add unmock. (#1)
    * Add unmock orb.
    
    * Dummy.
    
    * Mock httpbin.org.
    
    * Use GitHub API.
    
    * Read token from env.
    
    * Read environment variable from file.
    
    * Add fake env.
    
    * Add goget.
    
    * Add a Makefile for fun.
    
    * Add githubv3.
    
    * Try set unmock state.
    
    * Add name to step.
    
    * Use set_code.
    
    * Change cmd name.

--- get.go
+++ get.go
@@ -1,22 +1,53 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
-func makeGetCall() {
-	resp, err := http.Get("https://httpbin.org/get")
-	if err != nil {
-		log.Fatalln(err)
+func init() {
+	// loads values from .env into the system
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
 	}
+}
+
+func makeGetCall() (string, error) {
+
+	client := &http.Client{}
+
+	token, exists := os.LookupEnv("GITHUB_TOKEN")
+
+	if !exists {
+		log.Fatalln("Environment variable GITHUB_TOKEN not set")
+	}
+
+	req, err := http.NewRequest("GET", "https://api.github.com/user/repos", nil)
+	req.Header.Add("Accept", "application/vnd.github.v3+json")
+	req.Header.Add("Authorization", fmt.Sprintf("token %s", token))
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return "", err
+	}
+
 	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return "", fmt.Errorf("Invalid status %d", resp.StatusCode)
+	}
+
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatalln(err)
+		return "", err
 	}
 	log.Println(string(body))
+	return string(body), nil
 }
 
 func main() {
