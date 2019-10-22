eda9cce07320d53661b7bd2561781e37c9fdf0dc
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-10-22 10:28:40 +0300

    Use GitHub API.

--- get.go
+++ get.go
@@ -1,22 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
 
-func makeGetCall() {
-	resp, err := http.Get("https://httpbin.org/get")
+func makeGetCall() (string, error) {
+
+	client := &http.Client{}
+
+	token := "fake"
+
+	req, err := http.NewRequest("GET", "https://api.github.com/user/repos", nil)
+	req.Header.Add("Accept", "application/vnd.github.v3+json")
+	req.Header.Add("Authorization", fmt.Sprintf("token %s", token))
+	resp, err := client.Do(req)
+
 	if err != nil {
-		log.Fatalln(err)
+		return "", err
 	}
+
 	defer resp.Body.Close()
+
+	if resp.Status != "200" {
+		return "", fmt.Errorf("Invalid status %s", resp.Status)
+	}
+
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatalln(err)
+		return "", err
 	}
 	log.Println(string(body))
+	return string(body), nil
 }
 
 func main() {
